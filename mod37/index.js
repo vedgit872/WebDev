d4af1cfabb18baf20fc96d77ac3cb2462c8c2e39
vedgit872 <vedsmith18@gmail.com>
2025-03-10 21:05:11 +0530

    crud in mongoose

--- mod37/index.js
+++ mod37/index.js
@@ -15,5 +15,36 @@ const moviescheema = new mongoose.Schema({
     rating: String,
 })
 const Movies = mongoose.model("Movies", moviescheema);
-const amadeus = new Movies({ name: "Amadeus", year: 1986, score: 9.2, rating: 'r-16' });//creates the new amades instance of movie class of moviesschema
-
+// const amadeus = new Movies({ name: "Amadeus", year: 1986, score: 9.2, rating: 'r-16' });//creates the new amades instance of movie class of moviesschema
+Movies.insertMany([
+    { name: "Amadeus", year: 1986, score: 9.2, rating: 'r-16' },
+    {
+        name: "The Godfather",
+        year: 1972,
+        score: 9.1,
+        rating: "r-18"
+    },
+    {
+        name: "Forrest Gump",
+        year: 1994,
+        score: 8.8,
+        rating: "pg-13"
+    },
+    {
+        name: "The Dark Knight",
+        year: 2008,
+        score: 9.0,
+        rating: "r-13"
+    },
+    {
+        name: "Pulp Fiction",
+        year: 1994,
+        score: 8.9,
+        rating: "r-18"
+    }])
+    .then(() => {
+        console.log("many succesful");
+    })
+    .catch(() => {
+        console.log("error");
+    })
