c1c28c6bbe73c76b2ec7d1a176f5ea493063d7b1
vedgit872 <vedsmith18@gmail.com>
2025-04-15 21:58:50 +0530

    Where To apply State
    As High As needed but not higher. TO change the state in child element we can pass a  function to change state of  the parent element as a whole

--- mod67_onwards/state-inter-demo/src/App.jsx
+++ mod67_onwards/state-inter-demo/src/App.jsx
@@ -4,10 +4,27 @@ import Dumbo from './Dumbo'
 import ScoreKeeper from './ScoreKeeper'
 import EmojiClicker from "./EmojiClicker"
 import ScoreKeeperMulti from './ScoreKeeperMulti'
+import LuckyN from "./LuckyN";
+import { sum } from "./utils";
+import BoxGrid from "./BoxGrid";
+function lessThan4(dice) {
+  return sum(dice) < 4;
+}
+
+function allSameValue(dice) {
+  return dice.every((v) => v === dice[0]);
+}
 function App() {
   return (
     <>
-      <ScoreKeeperMulti />
+      <BoxGrid />
+      {/* <LuckyN winCheck={lessThan4} title="Roll less than 4" />
+      <LuckyN
+        winCheck={allSameValue}
+        numDice={3}
+        title="Roll the same number"
+      /> */}
+      {/* <ScoreKeeperMulti /> */}
       {/* <EmojiClicker /> */}
       {/* <ScoreKeeper /> */}
       {/* <Dumbo />
