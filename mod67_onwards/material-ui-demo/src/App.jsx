3b242ac629a8704a917c58a6026ace2fee69b2df
vedgit872 <vedsmith18@gmail.com>
2025-04-21 20:13:33 +0530

    Material Ui
    Material Ui helps use components and reduces the work to make components from scratch easier.

---
+++ mod67_onwards/material-ui-demo/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import RatingDemo from "./RatingDemo";
+import FormDemo from "./FormDemo";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <FormDemo />
+      <RatingDemo />
+    </div>
+  );
+}
+
+export default App;
